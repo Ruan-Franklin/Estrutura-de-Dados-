ec47ca9d9b82c32d5cdb509f53ef22e2e028b1b0
Ruan-Franklin <ruanfranklinmengo@gmail.com>
2026-02-16 18:01:19 -0300

    :sparkles: feat: adding stack class

---
+++ Stack, Queue, Deck/stack.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class Pilha:
+    def __init__(self, capacidade):
+        self.__capacidade = capacidade
+        self.__topo = -1
+        self.__valores = np.empty(self.__capacidade, dtype=int)
+
+    def __pilha_cheia(self):
+        if self.__topo == self.__capacidade -1:
+            return True
+        return False
+
+    def __pilha_vazia(self):
+        if self.__topo == -1:
+            return True
+        return False
+
+    def empilhar(self, valor):
+        if self.__pilha_cheia():
+            print("Pilha cheia")
+        self.__topo += 1
+        self.__valores[self.__topo] = valor
+    def desempilhar(self):
+        if self.__pilha_vazia():
+            print("Pilha vazia, não é possível desempilhar")
+        self.__topo -= 1
+
+    def ver_topo(self):
+        if self.__topo != -1:
+            return self.__valores[self.__topo]
+
+pilha = Pilha(5)
+
+pilha.empilhar(1)
+pilha.ver_topo()
