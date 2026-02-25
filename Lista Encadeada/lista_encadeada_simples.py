8d62a95b08644653134ee5fac71b626f8739c4ff
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2026-02-25 17:29:40 +0000

    :sparkles: feat: addung delete method for simply linked list

--- Lista Encadeada/lista_encadeada_simples.py
+++ Lista Encadeada/lista_encadeada_simples.py
@@ -6,9 +6,36 @@ class No:
         self.proximo = None
     
     def mostra_no(self):
-        print(self.valor)
+        print(self.valor) 
         
 
 class ListaEncadeada:
     def __init__(self):
         self.primeiro = None
+        
+    def insere_inicio(self, valor):
+        novo = No(valor)
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+        
+    def mostrar(self):
+        atual = self.primeiro
+        while atual != None:
+            atual.mostra_no()
+            atual = atual.proximo
+    
+    def excluir_inicio(self):
+        if self.primeiro == None:
+            print("Lista vazia")
+            return None
+        temp = self.primeiro
+        self.primeiro = self.primeiro.proximo
+        
+        return temp            
+            
+lista = ListaEncadeada()
+lista.insere_inicio(10)
+lista.insere_inicio(20)
+lista.mostrar()
+            
+        
