12ca1c44b2d48d993c0be3fdf809c63540c3ab44
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2026-03-02 19:45:10 +0000

    :sparkles: adding insert end method to doubly linked list

--- Lista Encadeada/lista_duplamente_encadeada.py
+++ Lista Encadeada/lista_duplamente_encadeada.py
@@ -23,6 +23,15 @@ class ListaDuplamenteEncadeada:
         novo.proximo = self.primeiro
         self.primeiro = novo
         
+    def insere_final(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.primeiro = novo
+        else:
+            self.ultimo.proximo = novo
+        self.novo.anterior = self.ultimo
+        self.ultimo = novo
+        
     def mostrar_frente(self):
         atual = self.primeiro
         while atual != None:
