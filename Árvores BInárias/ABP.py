36b0c9c6da569d026b1720512132ef8875a3a038
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2026-03-17 13:35:49 +0000

    :sparkles: feat: adding insert function to ABP tree

--- Árvores BInárias/ABP.py
+++ Árvores BInárias/ABP.py
@@ -13,3 +13,41 @@ class ABP:
         
     def _esta_vazia(self):
         return self.raiz is None
+    
+    def inserir(self, valor):
+        novo = No(valor)
+        
+        if self._esta_vazia():
+            self.raiz = novo
+            atual = self.raiz
+        else:
+            atual = self.raiz
+        while True:
+            pai = atual
+            if valor < atual.valor:
+                atual = atual.esquerda
+                if atual == None:
+                    pai.esquerda = novo
+                    return
+            else:
+                atual = atual.direita
+                if atual == None:
+                    pai.direita = novo
+                    return
+        
+
+
+arvore = ABP()
+arvore.inserir(53)
+arvore.inserir(30)
+arvore.inserir(14)
+arvore.inserir(39)
+arvore.inserir(9)
+arvore.inserir(23)
+arvore.inserir(34)
+arvore.inserir(49)
+arvore.inserir(72)
+arvore.inserir(61)
+arvore.inserir(84)
+arvore.inserir(79)
+print("Terminou?")
