437432c5434837c3588d4c87f981957eb1d44b01
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2026-03-19 13:02:21 +0000

    :sparkles: feat: finish exclude function for binary search tree

--- Árvores BInárias/ABP.py
+++ Árvores BInárias/ABP.py
@@ -59,7 +59,73 @@ class ABP:
             print(no.valor)
             self.em_ordem(no.direita)
             
+    def excluir(self, no):
+        if self._esta_vazia():
+            return
+        atual = self.raiz
+        pai = self.raiz
+        eh_esquerdo = True
+        while atual.valor != valor:
+            pai = atual
+            if valor < atual.valor:
+                eh_esquerdo = True
+                atual = atual.esquerdo
+            else:
+                eh_esquerdo = False
+                atual = atual.direita
+            if atual == None:
+                return False
+            
+            if atual.esquerda == None and atual.direita == None: 
+                if atual  == self.raiz:
+                    self.raiz = None
+                elif eh_esquerdo:
+                    pai.esquerda = None
+                else:
+                    pai.direita = None
+            
+            elif atual.direita == None:
+                if atual == self.raiz:
+                    self.raiz = atual.esquerda
+                elif eh_esquerdo:
+                    pai.esquerda = atual.esquerda
+                else:
+                    pai.direita = atual.esquerda
+            elif atual.esquerda == None:
+                if atual == self.raiz:
+                    self.raiz = atual.direita
+                elif eh_esquerdo:
+                    pai.esquerda = atual.direita
+                else:
+                    pai.direita = atual.direita
+            else:
+                sucessor = self.get_sucessor(atual)
+                if atual == self.raiz:
+                    self.raiz = sucessor
+                elif eh_esquerdo:
+                    pai.esquerda = sucessor
+                else:
+                    pai.direita = sucessor
+                sucessor.esquerda = atual.esquerda
+            return True
+                    
+        def get_sucessor(self, no):
+            pai_sucessor = no
+            sucessor = no
+            atual = no.direita
+            while atual != None:
+                pai_sucessor = sucessor
+                sucessor = atual
+                atual = atual.esquerda
+            if sucessor != no.direita:
+                pai_sucessor.esquerda = sucessor.direita
+                sucessor.direita = no.direita
+            return sucessor
+        
+                    
+            
     
+
     def pos_ordem(self, no):
         self.pos_ordem(no.esquerda)
         self.pos_ordem(no.direita)
@@ -80,5 +146,6 @@ arvore.inserir(72)
 arvore.inserir(61)
 arvore.inserir(84)
 arvore.inserir(79)
+arvore.excluir(14)
 
 arvore.pesquisar(30).mostra_no()
